e45f2d5411ce71962054fd41355e06a7deb7e9f3
Gamze Bayram <gamzebayram90@gmail.com>
2023-08-14 16:17:13 +0300

    added

---
+++ src/main/java/io/upschool/dto/ticket/TicketSaveRequest.java
@@ -0,0 +1,37 @@
+package io.upschool.dto.ticket;
+
+import io.upschool.entity.Flight;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TicketSaveRequest {
+
+    @Builder.Default
+    private boolean isDelete = false;
+    @NotBlank
+    private String passengerName;
+    @NotBlank
+    private String cardNumber;
+    private Double price;
+    private String flightNumber;
+
+
+
+
+
+
+
+
+
+
+
+
+}
